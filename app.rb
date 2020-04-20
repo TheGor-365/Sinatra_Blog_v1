466b80a656e98c7579ad88400afc5f654f5dca70
Gor <7495500@gmail.com>
2020-04-20 12:56:50 +0300

    Add posts erb and database

---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQlite::Database.new 'Lepro.sqlite'
+	@db.results_as_hash = true
+end
+
+before do
+	init_db
+end
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+end
+
+get '/new' do
+
+	erb :new
+end
+
+post '/new' do
+	content = params[:content]
+
+	erb :new
+end
